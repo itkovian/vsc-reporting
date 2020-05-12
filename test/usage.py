73b80c21d21d8355b7d1e1aeaf38347ecb8e5417
Andy Georges <itkovian@gmail.com>
2020-05-12 12:02:21 +0200

    feat: test file, no tests yet

---
+++ test/usage.py
@@ -0,0 +1,20 @@
+##
+# Copyright 2020-2020 Ghent University
+#
+# This file is part of vsc-reporting,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# All rights reserved.
+#
+##
+
+import logging
+
+from vsc.install.testing import TestCase
+
+class TestUsage(TestCase):
+    pass
