70ed18cca51380e9945b2bf83e7425cd4bbb8c62
Andy Georges <itkovian@gmail.com>
2020-05-12 11:59:44 +0200

    feat: create reporting package

---
+++ lib/vsc/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2020-2020 Ghent University
+#
+# This file is part of vsc-reporting,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# All rights reserved.
+#
+"""
+@author: Andy Georges (Ghent University)
+"""
+#the vsc namespace is used in different folders allong the system
+#so explicitly declare this is also the vsc namespace
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
